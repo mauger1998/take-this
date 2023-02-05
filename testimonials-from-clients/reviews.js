f43523580977cdce1143c33390d0c98228be666a
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2023-02-05 10:27:57 +0000

    cms for testimonials page

--- testimonials-from-clients/reviews.js
+++ testimonials-from-clients/reviews.js
@@ -13,3 +13,52 @@ document.addEventListener("click", (e) => {
         dropdown.classList.remove("active")
     })
 })
+
+// CMS
+let PROJECT_ID = "7mw9871z"
+let DATASET = "production" 
+let Query = encodeURIComponent('*[_type == "testimonials"]')
+let URL = "https://7mw9871z.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20%22testimonials%22%5D"
+
+fetch(URL)
+    .then((res) => res.json())
+    .then(({ result }) => {
+        let topCard = document.querySelectorAll(".top-card")
+        let profileName = document.querySelectorAll(".name")
+        let profileEvent = document.querySelectorAll(".event")
+        let review = document.querySelectorAll(".card > p")
+
+        result.forEach((result, index) => {
+            profileName[index].textContent = result.name
+            profileEvent[index].textContent = result.event
+            review[index].textContent = result.review
+        })
+
+        
+    })
+
+let QueryTwo = encodeURIComponent('*[_type == "review-text"]')
+let URLTWO = "https://7mw9871z.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20%22review-text%22%5D"
+
+fetch(URLTWO)
+    .then((res) => res.json())
+    .then(({ result }) => {
+        let photoParagraph = document.querySelector(".bye-wrapper span > p")
+
+        result.forEach((result, index) => {
+            photoParagraph.textContent = result.text
+        })
+    })
+
+let QueryThree = encodeURIComponent('*[_type == "review-hero-text"]')
+let URLTHREE = "https://7mw9871z.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20%22review-hero-text%22%5D"
+
+fetch(URLTHREE)
+    .then((res) => res.json())
+    .then(({ result }) => {
+        let heroText = document.querySelector(".hero-wrapper p")
+
+        result.forEach((result, index) => {
+            heroText.textContent = result.text
+        })
+    })
